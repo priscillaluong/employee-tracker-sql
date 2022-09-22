326b0916655f490c676a11b4018bf06ab6d43c20
P Luong <p-luong@live.co.uk>
2022-09-22 15:45:02 +0100

    added dependancies

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const fs = require('fs');
 const mysqlConnection = require('./db/connection');
+const cTable = require('console.table');
+const logo = require('asciiart-logo');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
