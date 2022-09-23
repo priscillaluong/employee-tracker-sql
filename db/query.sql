c7e501413c877a89d0a8e25574626fcae8afb9f2
P Luong <p-luong@live.co.uk>
2022-09-23 16:17:52 +0100

    updated sql queries

--- db/query.sql
+++ db/query.sql
@@ -4,7 +4,30 @@ SELECT
 roles.id AS role_id,
 roles.role_title, 
 roles.salary,
-department.department_name AS department
+department.department_name
 FROM roles
 JOIN department 
 ON roles.department_id = department.id;
+
+/* Joining roles.department_id with department name from department table */
+
+SELECT 
+a.id AS employee_id,
+a.first_name,
+a.last_name, 
+/* roles.role_title,  */
+/* department.department_name, */
+/* roles.salary, */
+b.first_name AS manager_firstname,
+b.last_name AS manager_lastname
+FROM employee a
+INNER JOIN employee b ON a.manager_id = b.id;
+/* JOIN roles ON a.role_id = roles.id; */
+/* JOIN department ON department.id = roles.department_id; */
+
+/* SELECT 
+*
+FROM employee
+JOIN roles ON employee.role_id = roles.id;
+INNER JOIN employee ON employee.manager_id = manager.id;
+JOIN department ON department.id = Roles.department_id; */
